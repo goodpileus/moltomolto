0c797dd6f1f352c0d72655ac55bfb766b9e6aea5
aidan <aidankquinlan@gmail.com>
2022-03-12 15:15:47 -0500

    favi + submit button

--- js/functions.js
+++ js/functions.js
@@ -17,3 +17,15 @@ randomPosition('.rand-b');
 randomPosition('.rand-c');
 randomPosition2('.rand-d');
 randomPosition2('.rand-e');
+
+const isEmpty = str => !str.trim().length;
+
+document.getElementById("mce-EMAIL").addEventListener("input", function() {
+  if( isEmpty(this.value) ) {
+    // console.log( "NAME is invalid (Empty)" )
+    $('#mc-embedded-subscribe').addClass('hide');
+  } else {
+    $('#mc-embedded-subscribe').removeClass('hide');
+    // console.log( `NAME value is: ${this.value}` );
+  }
+});
